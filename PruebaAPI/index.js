c75cb730841e9376fc833edc6a5fd06e29255209
Juan Carlos Chambi Python Dev <juan199963@gmail.com>
2022-03-22 23:46:41 -0500

    se agrego la API peliculas

--- PruebaAPI/index.js
+++ PruebaAPI/index.js
@@ -18,15 +18,68 @@
 // xhr.open('GET', '${API_URL}/users');
 // xhr.send();
 
+let pagina = 1;
+
+const btnAnterior = document.getElementById('btnAnterior');
+const btnSiguiente = document.getElementById('btnSiguiente');
+
+btnSiguiente.addEventListener('click', ()=>{
+    if(pagina < 1000){
+        pagina += 1;
+        cargarPeliculas();
+    }else{
+        alert('Llego al limite de peliculas')
+    }
+    
+})
+
+btnAnterior.addEventListener('click', ()=>{
+    if(pagina >1){
+        pagina -= 1;
+        cargarPeliculas();
+    }else{
+        alert('No hay hoja anterior')
+    }
+    
+})
+
+
+
+
 
 const cargarPeliculas = async() =>{
 
     try{
-        const respuesta = await fetch('https://api.themoviedb.org/3/movie/530?api_key=4a0d89fde128b0a74b9aee4518db97f0&language=es-MX');
+        const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=4a0d89fde128b0a74b9aee4518db97f0&language=es-MX&page=${pagina}`);
         console.log(respuesta);
-        const datos = await respuesta.json();
+        
+        if(respuesta.status === 200){
+            const datos = await respuesta.json();
 
-        console.log(datos.title);
+            let peliculas = '';
+
+            datos.results.forEach(pelicula =>{
+                peliculas +=
+                 `
+                 <div class = "pelicula">
+                    <img class = "poster" src ="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+                    <h3 class = "titulo">${pelicula.title}</h3>
+                 </div>
+
+                 
+                 `;
+            });
+
+            document.getElementById('contenedor').innerHTML = peliculas
+
+        }else if(respuesta.status === 401){
+            console.log('Pusiste la llave mal');
+        }else if(respuesta.status === 404){
+            console.log('La pelicula no existe');
+        }else{
+            console.log('Hubo un error desconocido.');
+        }
+        
     } catch(error){
         console.log(error);
     }
